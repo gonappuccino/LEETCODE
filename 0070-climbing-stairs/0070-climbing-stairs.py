b82240d338dc7b6c6a52834d8cafb05b8cea8a75
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-06-18 19:12:53 -0400

    Time: 0 ms (100%), Space: 18 MB (7.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        def dp(k):
+            if k <= 1:
+                return 1
+            if k in memo:
+                return memo[k]
+            memo[k] = dp(k-1) + dp(k-2)
+            return memo[k]
+        return dp(n)
