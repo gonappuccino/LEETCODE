28be9c176fe72488310a3292590cfb50a17f1d9f
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-07-31 18:30:02 -0400

    Time: 3 ms (49.84%), Space: 19.7 MB (39.73%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,24 +3,25 @@ class Solution:
         res = []
         inserted = False
 
-
         for interval in intervals:
-            # 왼쪽 벗어남
+            # outside - left
             if interval[1] < newInterval[0]:
                 res.append(interval)
-            # 오른쪽 벗어남 (new먼저 들어가고, interval append)
+            # outside - right
             elif interval[0] > newInterval[1]:
                 if not inserted:
                     res.append(newInterval)
                     inserted = True
                 res.append(interval)
-            # merge needed
+            # merge
             else:
                 newInterval[0] = min(newInterval[0], interval[0])
                 newInterval[1] = max(newInterval[1], interval[1])
-        # new interval이 가장 오른쪽
+
+            # newInterval = rightmost
         if not inserted:
             res.append(newInterval)
+            
         return res
 
                 
