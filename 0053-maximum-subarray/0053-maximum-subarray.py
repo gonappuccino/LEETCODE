ea60ed61eeae6bb349273e8dd0518cc848786dd5
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-07-31 13:57:18 -0400

    Time: 91 ms (28.89%), Space: 32.1 MB (66.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        current_sum = max_sum = nums[0]
+        #kadane's algorithm
+        #지금까지의 합이 음수면 버려
+        curr_sum = float('-inf')
+        max_sum = float('-inf')
 
-        for num in nums[1:]:
-            current_sum = max(num, current_sum + num)
-            max_sum = max(max_sum, current_sum)
+        for num in nums:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(curr_sum, max_sum)
         return max_sum
-                    
+
+        
+        
