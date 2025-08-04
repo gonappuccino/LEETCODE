6983cc25d107272f66b06814823e1506680701a7
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-08-04 17:52:56 -0400

    Time: 1916 ms (5.38%), Space: 21.6 MB (11.63%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #[-4,-1,-1,0,1,2]
         nums.sort()
-        res = []
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            complement = 0 - nums[i] #1
-            left = i+1 #3
-            right = len(nums)-1 #5
-            #print(complement, left, right)
+        res = set()
+        # -4 -1 -1 0 1 2
+        
+        for i in range(len(nums)):
+            left = i+1
+            right = len(nums) - 1
             while left < right:
-                if nums[left] + nums[right] == complement:
-                    #print('1')
-                    res.append([nums[i], nums[left], nums[right]])
-                    
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
-                    
-                    left += 1
+                if nums[i] + nums[left] + nums[right] == 0:
+                    res.add((nums[i], nums[left], nums[right]))
                     right -= 1
-                    #print(res)
-                elif nums[left] + nums[right] > complement:
-                    #print('2')
-                    right -= 1
-                else:
-                    #print('3')
                     left += 1
-        return res
+                elif nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
+                elif nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+
+        return [list(triplet) for triplet in res]
