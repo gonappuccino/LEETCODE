1efeee6d7884ab4aaa2b45d060d097ce50a82273
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-08-04 13:19:12 -0400

    Time: 134 ms (57.86%), Space: 20.4 MB (70.59%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,26 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        if not mat:
-            return []
-        
         rows, cols = len(mat), len(mat[0])
-        dist=[[-1 for _ in range(cols)] for _ in range((rows))]
-        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
-        
-        queue = deque()
+        dist = [[float('inf')]*cols for _ in range(rows)]
+        queue =deque()
 
         for i in range(rows):
             for j in range(cols):
                 if mat[i][j] == 0:
                     dist[i][j] = 0
-                    queue.append((i,j))
+                    queue.append((i, j))
 
-        while queue:
-            i, j = queue.popleft()
-            for direction in directions:
-                next_i, next_j = i + direction[0], j + direction[1]
-                if 0 <= next_i < rows and 0 <= next_j < cols and dist[next_i][next_j] == -1:
-                    dist[next_i][next_j] = dist[i][j] + 1
-                    queue.append((next_i, next_j))
-
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        while(queue):
+            i , j = queue.popleft()
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if(0 <= ni < rows and 0 <= nj < cols):
+                    if dist[ni][nj] > dist[i][j] + 1:
+                        dist[ni][nj] = dist[i][j] + 1
+                        queue.append((ni, nj))
         return dist
