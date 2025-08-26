de15e6be5b369b678fbeb25c742150b00a994755
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-08-26 13:54:41 -0400

    Time: 431 ms (96.57%), Space: 18.2 MB (21.82%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,38 +4,38 @@ class Solution:
 
         if len(word) > rows * cols:
             return False
-
-        cnt_board = Counter(ch for row in board for ch in row)
+        
+        cnt_board  = Counter(ch for row in board for ch in row)
         cnt_word = Counter(word)
+
         for ch, need in cnt_word.items():
             if cnt_board[ch] < need:
                 return False
+        
 
         def dfs(r, c, k):
-            # 마지막 글자 도달
+
             if k == len(word):
                 return True
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[k]:
+            if r < 0 or r >= rows or c < 0 or c >= cols or word[k] != board[r][c]:
                 return False
+            
 
             tmp = board[r][c]
             board[r][c] = '#'
 
-            found =(
-                dfs(r+1, c, k+1) or
+            found = (
                 dfs(r-1, c, k+1) or
-                dfs(r, c+1, k+1) or
-                dfs(r, c-1, k+1)
+                dfs(r+1, c, k+1) or
+                dfs(r, c-1, k+1) or
+                dfs(r, c+1, k+1) 
             )
+
             board[r][c] = tmp
             return found
-
+        
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] == word[0] and dfs(i, j, 0):
+                if dfs(i, j, 0):
                     return True
         return False
-            
-        
-        
-        
