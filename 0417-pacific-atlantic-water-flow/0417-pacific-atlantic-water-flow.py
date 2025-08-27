4cae2ec77dddb58ce6268d62edff2410b38b725f
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-08-27 10:04:59 -0400

    Time: 27 ms (84.28%), Space: 19.5 MB (46.28%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,36 +1,30 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # cells where water can reach both Pacific and Atlantic
-        # BFS: start from the ocean edges
-        # left, top = the Pacific
-        # right, bottom = the Atlantic
         if not heights or not heights[0]:
             return []
-        
         m, n = len(heights), len(heights[0])
+        pac_start = [[0, j] for j in range(n)] + [[i, 0] for i in range(m)]
+        atl_start = [[m-1, j] for j in range(n)] + [[i, n-1] for i in range(m)]
 
-        pac_start = [(0, j) for j in range(n)] + [(i, 0) for i in range(m)]
-        atl_start = [(m-1, j) for j in range(n)] + [(i, n-1) for i in range(m)]
         directions = [(-1,0), (1,0), (0,-1), (0,1)]
 
         def bfs(starts):
             vis = [[False]*n for _ in range(m)]
             q = deque(starts)
             for r, c in starts:
-                vis[r][c] = True
+                if not vis[r][c]:
+                    vis[r][c] = True
             
             while q:
                 r, c = q.popleft()
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    if (0 <= nr < m) and (0 <= nc < n) and not vis[nr][nc] and heights[r][c] <= heights[nr][nc] :
+                    if (0 <= nr < m) and (0 <= nc < n) and not vis[nr][nc] and heights[nr][nc] >= heights[r][c]:
                         vis[nr][nc] = True
                         q.append((nr,nc))
             return vis
-        pacific = bfs(pac_start)
-        atlantic = bfs(atl_start)
-
-        return [[r, c] for r in range(m) for c in range(n) if pacific[r][c] and atlantic[r][c]]
-
         
-        
+        pac = bfs(pac_start)
+        atl = bfs(atl_start)
+
+        return [[r,c] for r in range(m) for c in range(n) if pac[r][c] and atl[r][c]]
