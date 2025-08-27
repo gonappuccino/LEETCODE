e2d5647b49b54b48b5a2177371c0252ddeb07091
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-08-27 15:05:03 -0400

    Time: 171 ms (10.2%), Space: 50.3 MB (5.79%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,29 +2,30 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         if not heights or not heights[0]:
             return []
+        
         m, n = len(heights), len(heights[0])
-        pac_start = [[0, j] for j in range(n)] + [[i, 0] for i in range(m)]
-        atl_start = [[m-1, j] for j in range(n)] + [[i, n-1] for i in range(m)]
-
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        
+        pac_start = [[0, j] for j in range(n)] + [[i,0] for i in range(m)]
+        atl_start = [[m-1, j] for j in range(n)] + [[i,n-1] for i in range(m)]
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
         def bfs(starts):
-            vis = [[False]*n for _ in range(m)]
-            q = deque(starts)
+            visited = [[False]*n for r in range(m) for c in range(n)] 
             for r, c in starts:
-                if not vis[r][c]:
-                    vis[r][c] = True
-            
+                visited[r][c] = True
+            q = deque(starts)
+
             while q:
                 r, c = q.popleft()
+
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    if (0 <= nr < m) and (0 <= nc < n) and not vis[nr][nc] and heights[nr][nc] >= heights[r][c]:
-                        vis[nr][nc] = True
+                    if (0 <= nr < m) and (0 <= nc < n) and not visited[nr][nc] and heights[nr][nc] >= heights[r][c]:
+                        visited[nr][nc] = True
                         q.append((nr,nc))
-            return vis
+            return visited
         
         pac = bfs(pac_start)
         atl = bfs(atl_start)
 
-        return [[r,c] for r in range(m) for c in range(n) if pac[r][c] and atl[r][c]]
+        return [[r, c] for r in range(m) for c in range(n) if pac[r][c] and atl[r][c]]
