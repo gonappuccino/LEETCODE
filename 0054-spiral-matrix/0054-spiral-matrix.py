f5d24f931381fc1063164c9a9a8acc98bfbd75e3
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-08-27 14:36:33 -0400

    Time: 0 ms (100%), Space: 17.7 MB (61.87%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,33 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:
+        if not matrix or not matrix[0]:
             return []
 
-        result = []
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
+        m, n = len(matrix), len(matrix[0])
+        top, bottom = 0, m-1
+        left, right = 0, n-1
+        res = []
 
         while top <= bottom and left <= right:
-            # → 1. 왼 → 오
-            for j in range(left, right + 1):
-                result.append(matrix[top][j])
-            top += 1  # 위쪽 줄은 끝났으니까 한 칸 아래로
+            
+            for c in range(left, right+1):
+                res.append(matrix[top][c])
+            top += 1
+            
+            for r in range(top, bottom+1):
+                res.append(matrix[r][right])
+            right -= 1
 
-            # ↓ 2. 위 → 아래
-            for i in range(top, bottom + 1):
-                result.append(matrix[i][right])
-            right -= 1  # 오른쪽 줄은 끝났으니까 한 칸 왼쪽으로
 
-            # ← 3. 오 → 왼
             if top <= bottom:
-                for j in range(right, left - 1, -1):
-                    result.append(matrix[bottom][j])
+                for c in range(right, left-1, -1):
+                    res.append(matrix[bottom][c])
                 bottom -= 1
-
-            # ↑ 4. 아래 → 위
+            
             if left <= right:
-                for i in range(bottom, top - 1, -1):
-                    result.append(matrix[i][left])
+                for r in range(bottom, top-1, -1):
+                    res.append(matrix[r][left])
                 left += 1
-
-        return result
+        return res
