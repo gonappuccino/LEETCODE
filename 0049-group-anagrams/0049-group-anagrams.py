742686d39eaac224404e2076bd88dfe37cad3efc
ARON <76672910+gonappuccino@users.noreply.github.com>
2025-12-12 11:19:19 -0500

    Time: 10 ms (86.82%), Space: 20.8 MB (54.63%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,9 +3,8 @@ class Solution:
         group = defaultdict(list)
 
         for s in strs:
-            sorted_s = sorted(s)
-            sorted_s = ''.join(sorted_s)
-            group[sorted_s].append(s)
+            key = ''.join(sorted(s))
+            group[key].append(s)
         return list(group.values())
 
         
